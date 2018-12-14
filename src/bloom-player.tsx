b1e34724a812dc372325a0b5d129b3701f88e3d9
John Thomson <john_thomson@sil.org>
2018-12-14 14:23:48 -0600

    Pre-review cleanup

--- src/bloom-player.tsx
+++ src/bloom-player.tsx
@@ -5,15 +5,22 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../node_modules/style-scoped/scoped.js"; // maybe use .min.js after debugging?
 import "./bloom-player.css";
-//import * as ReactDOM from "react-dom";
+
+// BloomPlayer takes the URL of a folder containing a Bloom book. The file name
+// is expected to match the folder name.
+// It displays pages from the book and allows them to be turned by dragging.
+// On a wide screen, an option may be used to show the next and previous pages
+// beside the current one.
 
 interface IBloomPlayerProps {
-    url: string; // of the bloom book
+    url: string; // of the bloom book (folder)
     showContext?: string; // currently may be "no" or "yes"
 }
 interface IState {
-    pages: Array<string>;
-    styles: string;
+    pages: Array<string>; // of the book. First and last are empty in context mode.
+    styles: string; // concatenated stylesheets the book references or embeds.
+    // indicates current page, though typically not corresponding to the page
+    // numbers actually on the page.
     currentIndex: number;
 }
 export default class BloomPlayer extends React.Component<
@@ -26,8 +33,6 @@ IState
             currentIndex: 0
     };
 
-    //private rootElt : HTMLElement;
-
     private shouldShow3Pages(): boolean {
         // enhance: this may acquire a third state in which it responds to width.
         return this.props.showContext === "yes";
@@ -35,7 +40,7 @@ IState
 
     public componentDidMount() {
         var index = this.props.url.lastIndexOf("/");
-        var filename = this.props.url.substring(index + 1); // enhance: no slash??
+        var filename = this.props.url.substring(index + 1);
         var htmUrl = this.props.url + "/" + filename + ".htm"; // enhance: search directory if name doesn't match?
         axios.get(htmUrl).then(result => {
             const doc = document.createElement('html');
@@ -47,6 +52,7 @@ IState
                 (editable.snapshotItem(iedit) as HTMLElement).removeAttribute("contenteditable");
             }
 
+            // assemble the page content list
             var pages = doc.getElementsByClassName("bloom-page");
             var sliderContent = [];
             if (this.shouldShow3Pages()) {
@@ -54,7 +60,9 @@ IState
             }
             for (var i = 0; i < pages.length; i++) {
                 const page = pages[i];
-                const content = page; //page.getElementsByClassName("marginBox")[0];
+                const content = page;
+                // urls of images and videos (and eventually audio) need to be made
+                // relative to the original book folder, not the page we are embedding them into.
                 const srcElts = document.evaluate(".//*[@src]", content, null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
                 for (var j = 0; j < srcElts.snapshotLength; j++) {
                     const item = srcElts.snapshotItem(j) as HTMLElement;
@@ -70,6 +78,8 @@ IState
             if (this.shouldShow3Pages()) {
                 sliderContent.push(""); // blank page to fill the space right of last.
             }
+
+            // Assemble all the style rules from all the stylesheets the book references.
             const linkElts = document.evaluate(".//link[@href and @type='text/css']", doc, null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
             const promises = [];
             for (let i = 0; i < linkElts.snapshotLength; i++) {
@@ -78,13 +88,19 @@ IState
                 const fullHref = this.fullUrl(href);
                 promises.push(axios.get(fullHref));
             }
+            // The map trick here causes us to ignore errors and just use the stylesheets
+            // we can get.
             axios.all(promises.map(p => p.catch(()=> undefined))).then(results => {
                 let combinedStyle = "";
+
+                // start with embedded styles (typically before links in a bloom doc...)
                 const styleElts = document.evaluate(".//style[@type='text/css']", doc, null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
                 for (let k = 0; k < styleElts.snapshotLength; k++) {
                     const styleElt = styleElts.snapshotItem(k) as HTMLElement;
                     combinedStyle += styleElt.innerText;
                 }
+
+                // then add the stylesheet contents we just retrieved
                 results.forEach(result => {
                     if (result && result.data) {
                         combinedStyle += result.data;}
@@ -101,8 +117,6 @@ IState
         return this.props.url + "/" + url;
     }
 
-
-    // ref={renderedElement => (this.rootElt = renderedElement as HTMLElement)}
     public render() {
         // multiple classes help make rules more specific than those in the book's stylesheet
         // (which benefit from an extra attribute item like __scoped_N)
@@ -114,20 +128,22 @@ IState
                 infinite={false}
                 dots={true}
                 afterChange={index => this.setIndex(index)}>
-            {this.state.pages.map((slide, index) => {
-            return (
-                <div key={slide}  className={this.getItemClass(index)}>
-                    <style scoped>{this.state.styles}</style>
-                    <div dangerouslySetInnerHTML={{__html:slide}}>
-              </div>
-              </div>
-            );
-          })}
+                {this.state.pages.map((slide, index) => {
+                    return (
+                        <div key={slide}  className={this.getSlideClass(index)}>
+                            <style scoped>{this.state.styles}</style>
+                            <div dangerouslySetInnerHTML={{__html:slide}}>
+                    </div>
+                    </div>
+                    );
+                })}
             </Slider>
         </div>;
     }
 
-    private getItemClass(itemIndex: number): string {
+    // Get a class to apply to a particular slide. This is used to apply the
+    // contextPage class to the slides before and after the current one.
+    private getSlideClass(itemIndex: number): string {
         if (!this.shouldShow3Pages()) {
             return "";
         }
