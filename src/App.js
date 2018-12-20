1086489d50b91a5eb30eb47f4131b980af464410
John Thomson <john_thomson@sil.org>
2018-12-20 12:09:39 -0600

    Make actual web app that loads book from param

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BloomPlayer from './bloom-player';
 
 class App extends Component {
   render() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const url = urlParams.get('url');
+    let mainContent;
+    if (url) {
+      mainContent = <BloomPlayer showContext="yes" url={url}/>
+    } else {
+      mainContent = "Please use this page with param ?url= followed by a book URL."
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Bloom Player
         </header>
+        {mainContent}
       </div>
     );
   }
